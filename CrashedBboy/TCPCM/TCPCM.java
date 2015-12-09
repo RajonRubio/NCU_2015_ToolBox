128f146b50925ca0981c72c5db9f1e138665da37
CrashedBboy <crashedbboy@gmail.com>
2015-12-09 20:27:41 +0800

    TCP Client module

--- CrashedBboy/TCPCM/TCPCM.java
+++ CrashedBboy/TCPCM/TCPCM.java
@@ -7,72 +7,38 @@ public static final int TURNSOUTH = 2;
 public static final int TURNNORTH = 3;
 public static final int TURNWEST = 4;
 public static final int GET = 5;
+public static final int TCPPORT = 8000;
 
 public class TCPCM {  
-	private String serverAddress;       
-	private int serverPort;
-	private BufferedReader reader;           
 	private PrintStream writer;
 	private Socket sock;
 
-	public static void main(String[] args){
-		TCPCM client = new TCPCM();
-	}
-
 	public TCPCM (){ 
-		this.setServer();
-		this.connect(serverAddress, serverPort);
-
-		/** Create new thread to handle incoming message */
-		Thread readerThread = new Thread(new IncomingReader());  
-		readerThread.start();
+		this.sock = new Socket();
 	}
 
 	protected boolean connectServer(InetAddress serverIP) {
-		/**
-			return true:connection success,false: connection failed
-		*/
+		InetSocketAddress isa = new InetSocketAddress(serverIP.getHostAddress(), TCPPORT);
+		try {
+			this.sock.connect(isa, 10);
+			InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());  
+			this.writer = new PrintStream(sock.getOutputStream());
+		} catch(IOException e) {
+			System.out.println("I/O error occurs when creating the socket");
+			e.printStackTrace(System.out);
+			return false;
+		} catch(SocketTimeoutException e){
+			System.out.println("Connection timeout");
+			e.printStackTrace(System.out);
+			return false;
+		}
+		return true;
 	}
 
 	protected void inputMoves(int moveCode) {
-		/**
-			wrap the moveCode passed in and transmit to TCP server
-		*/
-	}
-
-	/** Set up host information */
-	private void setServer() {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Server IP Address: ");
-		this.serverAddress = sc.nextLine();
-		System.out.print("\nServer Port: ");
-		while(!sc.hasNextInt())
-			sc.next();
-		this.serverPort = sc.nextInt();
-	}
-
-	/** Create connection */
-	private void connect(String host, int port) {
-		try{
-			this.sock = new Socket(host, port);      
-			InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());  
-			this.reader = new BufferedReader(streamReader);    
-			this.writer = new PrintStream(sock.getOutputStream());
-		}catch(IOException e){
-			e.printStackTrace(System.out);
+		if(moveCode != null) {
+			this.writer.print(moveCode);
+			this.writer.flush();
 		}
 	}
-
-	public class IncomingReader implements Runnable{
-		public void run(){
-			String message;
-			try{
-				while ((message = reader.readLine()) != null){
-					/* parse */
-				}
-			}catch(Exception e){
-				e.printStackTrace();
-			}
-		}
-	} 
 }
