372473d1cc1a9ea4e529dc7fcb23a535f3ff8816
CrashedBboy <crashedbboy@gmail.com>
2015-12-09 21:55:37 +0800

    layout of server module

--- CrashedBboy/TCPCM/TCPCM.java
+++ CrashedBboy/TCPCM/TCPCM.java
@@ -2,23 +2,18 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-public static final int TURNEAST = 1;
-public static final int TURNSOUTH = 2;
-public static final int TURNNORTH = 3;
-public static final int TURNWEST = 4;
-public static final int GET = 5;
-public static final int TCPPORT = 8000;
 
 public class TCPCM {  
 	private PrintStream writer;
 	private Socket sock;
+	private static final int TCPPORT = 8000;
 
 	public TCPCM (){ 
 		this.sock = new Socket();
 	}
 
 	protected boolean connectServer(InetAddress serverIP) {
-		InetSocketAddress isa = new InetSocketAddress(serverIP.getHostAddress(), TCPPORT);
+		InetSocketAddress isa = new InetSocketAddress(serverIP.getHostAddress(), this.TCPPORT);
 		try {
 			this.sock.connect(isa, 10);
 			InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());  
--- CrashedBboy/TCPSM/TCPSM.java
+++ CrashedBboy/TCPSM/TCPSM.java
@@ -3,27 +3,27 @@ import java.net.*;
 import java.util.*;
 
 public class TCPServer{
-	private int listenPort;
+	private static final int LISTENPORT = 8000;
 	private static ServerSocket serverSock;
-	private int[] treasure;
-	private final static String[] treasureName = {"A", "B", "C"};
-
-	public static void main (String args[]){
-		TCPServer server = new TCPServer();
-		server.startListen();
-	}
+	private vector<String> clientIPTable;
 
 	public TCPServer() {
-		this.treasure = new int[]{0, 0, 0};
-		this.startListen();
 		int connNum = 0;
-		Thread logThread = new Thread(new LogHandler());
-		logThread.start();
-		while (connNum < 2) {
+		clientIPTable = new vector<String>();
+		serverSock = null;
+	}
+
+	protected void initTCPServer() {
+		try {
+			this.serverSock = new ServerSocket(this.listenPort);  
+		} catch (Exception e) {
+			e.printStackTrace(System.out);
+		}
+		while (true) {
 			try {
 					Socket s = serverSock.accept();    
-					System.out.println("Get one connection from " + s.getInetAddress());
 					connNum += 1;
+					clientIPTable.add(s.getInetAddress());
 					Thread t = new Thread(new ConnectionHandler(s, connNum)); 
 					t.start();           
 			} catch (Exception e) {
@@ -32,23 +32,10 @@ public class TCPServer{
 		}
 	}
 
-	private void startListen() {
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Listen Port: ");
-		this.listenPort = sc.nextInt();
-		try {
-			this.serverSock = new ServerSocket(this.listenPort);  
-			System.out.println("Listen on port " + this.listenPort + " ...");
-		} catch (Exception e) {
-			e.printStackTrace(System.out);
-		}
-	}
-
 	public class ConnectionHandler implements Runnable{   
 
 		private Socket sock;
 		private BufferedReader reader;
-		private PrintStream writer;
 		private int connectionID;
 
 		public ConnectionHandler (Socket s, int id){
@@ -57,7 +44,6 @@ public class TCPServer{
 				this.connectionID = id;
 				InputStreamReader isReader = new InputStreamReader(this.sock.getInputStream()); 
 				this.reader = new BufferedReader(isReader);
-				this.writer = new PrintStream(this.sock.getOutputStream());  
 			} catch (Exception e) {
 				e.printStackTrace(System.out);
 			} 
@@ -65,53 +51,11 @@ public class TCPServer{
 
 		public void run(){
 			String message;
-			Random ran = new Random();
 			try {
 				while ((message=reader.readLine()) != null){   
-					String[] msgArray = message.split(" ");
-					if (msgArray[0].equals("GET")) {
-						this.get(msgArray[1]);
-					} else if (msgArray[0].equals("RELEASE")) {
-						this.release(msgArray[1]);
-					}
+					/* parse and call CDC */
 				}
 			} catch (Exception e){
-				e.printStackTrace(System.out);
-			}
-		}
-
-		private void get(String name) {
-			try {
-				Random ran = new Random();
-				int treasureID = Arrays.asList(treasureName).indexOf(name);
-				if (treasureID != -1 && treasure[treasureID] == 0/* && ran.nextInt(2) == 1*/) {
-						treasure[treasureID] = this.connectionID;
-						this.writer.println("YES " + treasureName[treasureID]);
-				} else {
-						this.writer.println("NO " + treasureName[treasureID]);
-				}
-				writer.flush();
-			} catch (Exception e) {
-				e.printStackTrace(System.out);
-			}
-		}
-
-		private void release(String name) {
-			int treasureID = Arrays.asList(treasureName).indexOf(name);
-			treasure[treasureID] = 0;
-		}
-	}
-
-	public class LogHandler	implements Runnable {
-		public void run() {
-			try {
-				while (true) {
-					System.out.println("A " + (treasure[0]==0?"NO ":"YES ") + treasure[0]);
-					System.out.println("B " + (treasure[1]==0?"NO ":"YES ") + treasure[1]);
-					System.out.println("C " + (treasure[2]==0?"NO ":"YES ") + treasure[2]);
-					Thread.sleep(3000);
-				}
-			} catch (Exception e) {
 				e.printStackTrace(System.out);
 			}
 		}
