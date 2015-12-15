58478284899fd0fddc0a3bde8f8617d676eab564
CrashedBboy <crashedbboy@gmail.com>
2015-12-15 23:53:52 +0800

    client singleton

--- CrashedBboy/TCPCM/TCPClient.java
+++ CrashedBboy/TCPCM/TCPClient.java
@@ -1,39 +1,39 @@
+package TCPCM;
 import java.io.*;
 import java.net.*;
 import java.util.*;
 
-
-public class TCPCM {  
+public class TCPClient {  
 	private PrintStream writer;
 	private Socket sock;
 	private static final int TCPPORT = 8000;
+	private static TCPClient client = null;
 
-	public TCPCM (){ 
+	private TCPClient (){ 
 		this.sock = new Socket();
 	}
 
-	protected boolean connectServer(InetAddress serverIP) {
+	public static TCPClient getClient() {
+		if (client == null) {
+			client = new TCPClient();
+		}
+		return client;
+	}
+
+	public boolean connectServer(InetAddress serverIP) {
 		InetSocketAddress isa = new InetSocketAddress(serverIP.getHostAddress(), this.TCPPORT);
 		try {
 			this.sock.connect(isa, 10);
 			InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());  
 			this.writer = new PrintStream(sock.getOutputStream());
-		} catch(IOException e) {
-			System.out.println("I/O error occurs when creating the socket");
-			e.printStackTrace(System.out);
-			return false;
-		} catch(SocketTimeoutException e){
-			System.out.println("Connection timeout");
-			e.printStackTrace(System.out);
+		} catch(Exception e) {
 			return false;
 		}
 		return true;
 	}
 
-	protected void inputMoves(int moveCode) {
-		if(moveCode != null) {
-			this.writer.print(moveCode);
-			this.writer.flush();
-		}
+	public void inputMoves(int moveCode) {
+		this.writer.print(moveCode);
+		this.writer.flush();
 	}
 }
