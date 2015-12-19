deb935d840c3ded805032f63fdf918a44be609c6
jxcodetw <jxcode.tw@gmail.com>
2015-12-19 15:27:02 +0800

    tcpcm init

--- src/TCPCM/TCPCM.java
+++ src/TCPCM/TCPCM.java
@@ -1,12 +1,25 @@
 package TCPCM;
 
+import java.io.*;
+import java.net.*;
+import java.util.*; 
+
 public class TCPCM {
+	private final int TCPPORT = 8000;
+	private Socket socket;
+	
 	public TCPCM() {
-		
+		this.socket = null;
 	}
 	
-	public void connectServer() {
-		
+	public boolean connectServer(String serverIP) {
+		try {
+			this.socket = new Socket(serverIP, this.TCPPORT);
+		} catch(Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 	
 	public void inputMoves() {
