16f7a63d74d81974f998f41af9c75a1e56963423
David Lin <davidlin12tw@gmail.com>
2015-12-19 22:52:14 +0800

    finish SDM

--- src/TCPCM/TCPCM.java
+++ src/TCPCM/TCPCM.java
@@ -1,76 +1,27 @@
 package TCPCM;
 
-import java.io.*;
-import java.net.*;
-
-import SETTINGS.TCP; 
-
 public class TCPCM {
-	
-	private ObjectOutputStream writer = null;
-	private ObjectInputStream reader = null;
-	
 	public TCPCM() {
 		
 	}
 	
-	public boolean connectServer(String serverIP, String nickname) {
-		try {
-			Socket socket = new Socket(serverIP, TCP.PORT);
-			writer = new ObjectOutputStream(socket.getOutputStream());
-			reader = new ObjectInputStream(socket.getInputStream());
-			writer.writeObject(Protocols.Action.CH_NAME);
-			writer.writeObject(nickname);
-			writer.flush();
-			new Handler(socket).start();
-		} catch(Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;
-	}
-	
-	/*
-	 * move attack
-	 */
-	public void keyChange(Protocols.Action action) {
-		try {
-			writer.writeObject(action);
-			writer.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void chooseTeam(Protocols.Team team) {
-		try {
-			writer.writeObject(Protocols.Action.CH_TEAM);
-			writer.writeObject(team);
-			writer.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void chooseRole(Protocols.Role role) {
-		try {
-			writer.writeObject(Protocols.Action.CH_ROLE);
-			writer.writeObject(role);
-			writer.flush();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private class Handler extends Thread {
-		private Socket socket = null;
+	public void connectServer() {
 		
-		Handler(Socket socket) {
-			this.socket = socket;
-		}
+	}
+	
+	public void inputMoves() {
 		
-		@Override
-		public void run() {
-		}
+	}
+	
+	public void keyShootPressed() {
+		
+	}
+	
+	public void giveTeam() {
+		
+	}
+	
+	public void giveJob() {
+		
 	}
 }
